36986c40c1daf078b322e8eebd91c257964f1e44
Vijay <your-email@example.com>
2026-09-16 15:36:14 +0530

    Add React Docker configuration

---
+++ frontend/src/scripts/config.js
@@ -0,0 +1,10 @@
+const config = {
+  IP: "192.168.0.151",
+  WEBSOCKET_PORT: "9091",
+  PORT: 3001,
+  CMD_VEL_TOPIC: "/cmd_vel",
+  ODOM_TOPIC: "/odom",
+  POSE_TOPIC: "/amcl_pose",
+  SECRET_KEY: "Entered_taurus_app_123",
+};
+export default config;
